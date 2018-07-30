885692cff7fdd41549798c2cafd7be8ddfa187fa
prateek <prateek@localhost.localdomain>
2018-07-30 21:44:36 +0530

    added calculatoe client calls

--- src/main/java/com/proto/calculator/CalculatorClient.java
+++ src/main/java/com/proto/calculator/CalculatorClient.java
@@ -7,6 +7,8 @@ import io.grpc.ManagedChannelBuilder;
 
 public class CalculatorClient {
 	public static void main(String[] args) {
+		int num1= 10, num2= 100;
+		
 		System.out.println("Running GRPC Greeting Client....");
 
 		/* Create a insecure(plaintext) channel */
@@ -15,11 +17,17 @@ public class CalculatorClient {
 		/* Create a synchronous stub */
 		CalculatorBlockingStub syncClient = CalculatorGrpc.newBlockingStub(channel);
 		
-		InputNumber request = InputNumber.newBuilder().setNum1(1).setNum2(2).build();
+		InputNumber request = InputNumber.newBuilder().setNum1(num2).setNum2(num1).build();
 		OutputNumber response = syncClient.addition(request);
 		
 		/* Display results */
-		System.out.println(response.getNum());
+		System.out.println("Addition of "+num1+" and " +num2+" is "+ response.getNum());
+		response = syncClient.subraction(request);
+		System.out.println("Subtraction of "+num1+" and " +num2+" is "+ response.getNum());
+		response = syncClient.multiply(request);
+		System.out.println("Multiplication of "+num1+" and " +num2+" is "+ response.getNum());
+		response = syncClient.divide(request);
+		System.out.println("Division of "+num1+" and " +num2+" is "+ response.getNum());
 		
 		/* Shutdown channel */
 		System.out.println("Shutting down channel");
